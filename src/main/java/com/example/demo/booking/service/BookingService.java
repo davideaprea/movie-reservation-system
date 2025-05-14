f704ee8f316922cc44e8881d973e512badab0779
daviaprea <apredav@gmail.com>
2025-05-14 12:36:21 +0200

    method refactor

--- src/main/java/com/example/demo/booking/service/BookingService.java
+++ src/main/java/com/example/demo/booking/service/BookingService.java
@@ -26,14 +26,7 @@ public class BookingService {
 
     @Transactional
     public List<Booking> create(BookingDto dto, long userId, long scheduleId) {
-        List<Long> seatIds = dto.seatIds();
-        List<SeatDetail> selectedSeats = seatDao.findAll(seatIds);
-
-        if (selectedSeats.size() != seatIds.size()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Seat not found.");
-        }
-
-        selectedSeats.sort(Comparator.comparingInt(SeatDetail::seatNumber));
+        List<SeatDetail> selectedSeats = getSelectedSeats(dto.seatIds());
 
         checkRow(selectedSeats);
         checkSeatsAdjacency(selectedSeats);
@@ -42,15 +35,14 @@ public class BookingService {
 
         checkSeatHall(selectedSeats, hallId);
 
-        List<Booking> bookings = new ArrayList<>();
-
-        for(SeatDetail seat : selectedSeats) {
-            bookings.add(Booking.create(
-                    userId,
-                    seat.id(),
-                    scheduleId
-            ));
-        }
+        List<Booking> bookings = selectedSeats
+                .stream()
+                .map(seat -> Booking.create(
+                        userId,
+                        seat.id(),
+                        scheduleId
+                ))
+                .toList();
 
         return StreamSupport
                 .stream(bookingDao.saveAll(bookings).spliterator(), false)
@@ -58,11 +50,11 @@ public class BookingService {
     }
 
     private void checkSeatsAdjacency(List<SeatDetail> selectedSeats) {
-        for(int i = 1; i < selectedSeats.size(); i++) {
+        for (int i = 1; i < selectedSeats.size(); i++) {
             int curr = selectedSeats.get(i).seatNumber();
             int prev = selectedSeats.get(i - 1).seatNumber();
 
-            if(curr - prev != 1) {
+            if (curr - prev != 1) {
                 throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Seats must be distinct and adjacent.");
             }
         }
@@ -83,7 +75,7 @@ public class BookingService {
                 .stream()
                 .allMatch(seat -> seat.hallId() == hallId);
 
-        if(!areSeatsFromScheduleHall) {
+        if (!areSeatsFromScheduleHall) {
             throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Seats must be in the schedule hall.");
         }
     }
@@ -92,5 +84,17 @@ public class BookingService {
         return scheduleDao
                 .findScheduleHallId(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Schedule not found."));
+    }
+
+    private List<SeatDetail> getSelectedSeats(List<Long> seatIds) {
+        List<SeatDetail> selectedSeats = seatDao.findAll(seatIds);
+
+        if (selectedSeats.size() != seatIds.size()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Seat not found.");
+        }
+
+        selectedSeats.sort(Comparator.comparingInt(SeatDetail::seatNumber));
+
+        return selectedSeats;
     }
 }
