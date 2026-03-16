84630f0da2f67ab5edee3e842edeab78f72cef1d
daviaprea <apredav@gmail.com>
2026-03-16 10:00:38 +0100

    query schedules by filters controller endpoint

--- src/main/java/com/mrs/app/schedule/controller/ScheduleController.java
+++ src/main/java/com/mrs/app/schedule/controller/ScheduleController.java
@@ -2,12 +2,15 @@ package com.mrs.app.schedule.controller;
 
 import com.mrs.app.schedule.dto.ScheduleResponse;
 import com.mrs.app.schedule.dto.ScheduleCreateRequest;
+import com.mrs.app.schedule.dto.SchedulesGetFilters;
 import com.mrs.app.schedule.service.ScheduleService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -21,5 +24,10 @@ public class ScheduleController {
                 scheduleService.create(dto),
                 HttpStatus.CREATED
         );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScheduleResponse>> findByFilters(@ModelAttribute SchedulesGetFilters filters) {
+        return new ResponseEntity<>(scheduleService.findByFilters(filters), HttpStatus.OK);
     }
 }
--- src/main/java/com/mrs/app/schedule/dao/ScheduleSeatDAO.java
+++
@@ -1,7 +0,0 @@
-package com.mrs.app.schedule.dao;
-
-import com.mrs.app.schedule.entity.ScheduleSeat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleSeatDAO extends JpaRepository<ScheduleSeat, Long> {
-}
