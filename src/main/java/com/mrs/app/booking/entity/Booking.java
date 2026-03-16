9a19cf4c4a1d18c30f9607a83764bbf3d050587f
daviaprea <apredav@gmail.com>
2026-03-16 11:29:57 +0100

    entity refactor

--- src/main/java/com/mrs/app/booking/entity/Booking.java
+++ src/main/java/com/mrs/app/booking/entity/Booking.java
@@ -3,7 +3,6 @@ package com.mrs.app.booking.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Builder(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -14,9 +13,9 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private Long scheduleId;
+
     @Column(nullable = false, unique = true)
     private Long scheduleSeatId;
-
-    @Column(nullable = false)
-    private Long userId;
 }
