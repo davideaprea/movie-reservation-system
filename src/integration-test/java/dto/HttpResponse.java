b7abf43d11ca00aa19c76cc3be35d6a70f8267ea
daviaprea <apredav@gmail.com>
2026-03-18 18:08:44 +0100

    schedule tests setup

--- src/integration-test/java/dto/HttpResponse.java
+++
@@ -1,9 +0,0 @@
-package dto;
-
-import org.springframework.http.HttpStatus;
-
-public record HttpResponse<T>(
-        T body,
-        HttpStatus status
-) {
-}
--- src/integration-test/java/schedule/ScheduleControllerTest.java
+++ src/integration-test/java/schedule/ScheduleControllerTest.java
@@ -1,32 +1,53 @@
 package schedule;
 
+import com.mrs.app.schedule.dao.ScheduleDAO;
 import com.mrs.app.schedule.dto.ScheduleCreateRequest;
 import com.mrs.app.schedule.dto.ScheduleResponse;
-import dto.HttpResponse;
+import com.mrs.app.schedule.entity.Schedule;
+import com.mrs.app.shared.exception.ConflictingResourceError;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.client.RestTestClient;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Map;
 
 public class ScheduleControllerTest {
-    private ScheduleClient scheduleClient;
+    private RestTestClient restTestClient;
+    private ScheduleDAO scheduleDAO;
 
     @SneakyThrows
     @Test
-    void test() {
+    void givenValidPayload_whenCreatingSchedule_thenStatusCreated() {
         ScheduleCreateRequest request = new ScheduleCreateRequest(
                 1,
                 1,
                 LocalDateTime.now().plusDays(1),
                 Map.of("STANDARD", BigDecimal.valueOf(5))
         );
-        HttpResponse<ScheduleResponse> response = scheduleClient.create(request, ScheduleResponse.class);
 
-        Assertions.assertEquals(HttpStatus.CREATED, response.status());
-        Assertions.assertNotNull(response.body());
+        restTestClient.post().body(request).exchange()
+                .expectStatus().isCreated()
+                .expectBody(ScheduleResponse.class);
+    }
+
+    @SneakyThrows
+    @Test
+    void givenConflictingTimeRange_whenCreatingSchedule_thenStatusConflict() {
+        LocalDateTime scheduleStartTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime scheduleEndTime = scheduleStartTime.plusHours(2);
+        Schedule schedule = scheduleDAO.save(new Schedule(null, 1L, null, scheduleStartTime, scheduleEndTime));
+        ScheduleCreateRequest conflictingRequest = new ScheduleCreateRequest(
+                1,
+                1,
+                scheduleStartTime,
+                Map.of("STANDARD", BigDecimal.valueOf(5))
+        );
+
+        restTestClient.post().body(conflictingRequest).exchange()
+                .expectStatus().isEqualTo(HttpStatus.CONFLICT)
+                .expectBody(ConflictingResourceError.class);
     }
 }
