644f7df022c26ff352f650b5cc66bd95f390db79
daviaprea <apredav@gmail.com>
2026-03-20 15:04:43 +0100

    payment flow test setup

--- src/integration-test/java/module/OrderTest.java
+++ src/integration-test/java/module/OrderTest.java
@@ -1,6 +1,8 @@
 package module;
 
 import annotation.ContainerizedContextTest;
+import com.mrs.app.booking.entity.Booking;
+import com.mrs.app.booking.entity.SeatReservation;
 import com.mrs.app.booking.repository.BookingDAO;
 import com.mrs.app.hall.entity.Hall;
 import com.mrs.app.hall.entity.SeatType;
@@ -10,11 +12,15 @@ import com.mrs.app.movie.entity.Movie;
 import com.mrs.app.movie.repository.MovieDAO;
 import com.mrs.app.order.dao.OrderDAO;
 import com.mrs.app.order.dto.HTTPOrderCreateRequest;
+import com.mrs.app.order.dto.OrderCompletionResponse;
 import com.mrs.app.order.dto.OrderCreateResponse;
-import com.mrs.app.order.entity.Order;
+import com.mrs.app.payment.entity.Completion;
+import com.mrs.app.payment.entity.Payment;
+import com.mrs.app.payment.repository.CompletionDAO;
 import com.mrs.app.payment.repository.PaymentDAO;
 import com.mrs.app.schedule.dao.ScheduleDAO;
 import com.mrs.app.schedule.entity.Schedule;
+import com.mrs.app.schedule.entity.ScheduleSeat;
 import factory.HallFactory;
 import factory.MovieFactory;
 import factory.ScheduleFactory;
@@ -24,6 +30,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -46,6 +53,8 @@ public class OrderTest {
     private PaymentDAO paymentDAO;
     @Autowired
     private BookingDAO bookingDAO;
+    @Autowired
+    private CompletionDAO completionDAO;
 
     private Schedule schedule;
 
@@ -60,9 +69,10 @@ public class OrderTest {
     @SneakyThrows
     @Test
     void test() {
+        List<ScheduleSeat> selectedSeats = schedule.getSeats().subList(0, 2);
         HTTPOrderCreateRequest request = new HTTPOrderCreateRequest(
                 schedule.getId(),
-                List.of(schedule.getSeats().getFirst().getId())
+                selectedSeats.stream().map(ScheduleSeat::getId).toList()
         );
         OrderCreateResponse response = restTestClient.post().uri("/orders")
                 .body(request).exchange()
@@ -72,10 +82,37 @@ public class OrderTest {
 
         assert response != null;
 
-        Order order = orderDAO.findById(response.id()).get();
-
         assertThat(orderDAO.count()).isEqualTo(1);
+
+        Payment payment = paymentDAO.findById(response.payment().id()).get();
+
         assertThat(paymentDAO.count()).isEqualTo(1);
+        assertThat(payment.getGatewayOrderId()).isNotBlank();
+        assertThat(payment.getPrice()).isEqualTo(selectedSeats.stream()
+                .map(ScheduleSeat::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add));
+
+        Booking booking = bookingDAO.findById(response.booking().id()).get();
+
         assertThat(bookingDAO.count()).isEqualTo(request.seatIds().size());
+        assertThat(booking.getScheduleId()).isEqualTo(schedule.getId());
+        assertThat(booking.getSeatReservations().size()).isEqualTo(selectedSeats.size());
+        assertThat(booking.getSeatReservations())
+                .extracting(SeatReservation::getId)
+                .containsExactlyInAnyOrderElementsOf(request.seatIds());
+
+        OrderCompletionResponse completionResponse = restTestClient.patch().uri("/orders/" + response.id())
+                .body(request).exchange()
+                .expectStatus().isCreated()
+                .expectBody(OrderCompletionResponse.class)
+                .returnResult().getResponseBody();
+
+        assert completionResponse != null;
+
+        Completion completion = completionDAO.findById(completionResponse.id()).get();
+
+        assertThat(completionDAO.count()).isEqualTo(1);
+        assertThat(completion.getGatewayCompletionId()).isNotBlank();
+        assertThat(completion.getPayment().getId()).isEqualTo(payment.getId());
     }
 }
