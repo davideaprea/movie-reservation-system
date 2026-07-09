b70c77bbc605fac666031556bef50dc5691bf2c2
Davide Aprea <davide.aprea@itcubeconsulting.it>
2026-07-09 12:25:53 +0200

    unused code removal

--- src/main/java/com/mrs/app/location/repository/AddressRepository.java
+++ src/main/java/com/mrs/app/location/repository/AddressRepository.java
@@ -1,10 +1,7 @@
 package com.mrs.app.location.repository;
 
 import com.mrs.app.location.entity.Address;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-    Page<Address> findAllByCityId(Pageable pageable, long cityId);
 }
